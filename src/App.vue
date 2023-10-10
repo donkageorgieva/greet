4b2de3ff50f8cb374a903872c6774a868b10ce59
Donka <DoniGeorgieva16@gmail.com>
2023-10-10 14:05:17 +0300

    Reusable card

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <script>
-import axios from "axios";
+// import axios from "axios";
+import BaseCard from "./components/UI/BaseCard/BaseCard.vue";
 export default {
+  components: { BaseCard },
   mounted() {
-    axios.get("/api/wp-json/wc/store/products?page=1").then((response) => {
-      console.log(response);
-    });
+    // axios.get("/api/wp-json/wc/store/products?page=1").then((response) => {
+    //   console.log(response);
+    // });
   },
 };
 </script>
 
-<template><h1>Greet App</h1></template>
+<template>
+  <h1>Greet App</h1>
+  <base-card
+    :imgSrc="'https://greet.bg/wp-content/uploads/2022/04/0631479001644578128_big.jpg'"
+    :category="'Инфлуенсъри'"
+    :name="'Виктор Стоянов &#8211; Ергенът'"
+    :description="'<p>Участник в риалити шоуто &#8220;Ергенът&#8221;, Виктор е истински джентълмен. Заяви видео, записано лично от него, за да зарадваш свои близки с неповторима емоция.</p>'"
+  ></base-card>
+</template>
 
-<style scoped></style>
+<style></style>
---
+++ src/components/UI/BaseCard/BaseCard.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  props: {
+    imgSrc: String,
+    name: String,
+    description: String,
+    category: String,
+  },
+};
+</script>
+
+<template>
+  <article>
+    <img :src="imgSrc" />
+    <h2>{{ name }}</h2>
+    <h4>{{ category }}</h4>
+    <span v-html="description"></span>
+
+    <button>Add to Cart</button>
+  </article>
+</template>
+
+<style scoped></style>
