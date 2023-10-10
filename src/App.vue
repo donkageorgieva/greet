107a6e84cd39a7b5e7b52cbffa94ca5f7bb36c8e
Donka <DoniGeorgieva16@gmail.com>
2023-10-10 13:46:47 +0300

    Init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+import axios from "axios";
+export default {
+  mounted() {
+    axios.get("/api/wp-json/wc/store/products?page=1").then((response) => {
+      console.log(response);
+    });
+  },
+};
+</script>
+
+<template><h1>Greet App</h1></template>
+
+<style scoped></style>
